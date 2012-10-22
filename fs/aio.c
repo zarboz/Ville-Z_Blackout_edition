79b8ed8d1013ee81050f1604017b00a9e78bd68b
zarboz <zarboz@gmail.com>
2012-10-22 14:53:52 -0600

    Chagning blk_plug function

--- fs/aio.c
+++ fs/aio.c
@@ -1622,7 +1622,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i;
-	struct blk_plug plug;
+	
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1639,7 +1639,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		return -EINVAL;
 	}
 
-	blk_start_plug(&plug);
+	
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
